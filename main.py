af099796ce6f8d7d836297dda91f237966e5b21f
Kevin <1348675040@qq.com>
2023-04-07 15:14:03 +0800

    finish function of tools_checkDif

--- main.py
+++ main.py
@@ -2,31 +2,38 @@
 Program:
 	该程序可以协助识别网址中的不同部分，以便加快爬虫项目的开发。
 History:
-	2023/4/6	Shane	First release
+	2023/4/7	Shane	First release
+	finish function of tools_checkDif
 '''
 
 def tools_checkDif(url1, url2):
     '''对比两个网址，并输出其网址差异的地方'''
-    k = 0
-    for i in url1:
-        for j in url2:
-            if i == j:
-                k += 1
-                #相同值剔除
-                modUrl1.pop(k)     #pop需要传入int型变量
-                modUrl2.pop(k)
-                break
-            else:
-                #如果出现不同值，检测到数值相同时为止
-                sameFlag = 0
-                print(f'i:{i}\nj:{j}')
+    firstNum, endNum = 0, 0
+    saveNum = 2
+    modUrl1 = list (url1)
+    modUrl2 = list (url2)
+
+    while modUrl1[firstNum] == modUrl2[firstNum]:
+        firstNum += 1
+    while modUrl1 [-endNum-1] == modUrl2 [-endNum-1]:
+        endNum += 1
+    #删除首端、末端相同值，并保留saveNum个数
+    del modUrl1[0:firstNum - saveNum]
+    del modUrl2 [0:firstNum - saveNum]
+    if endNum != 0 and endNum>=saveNum:
+        del modUrl1 [-endNum+saveNum : ]    #     del modUrl2 [-endNum+saveNum : ]
+        del modUrl2 [-endNum+saveNum : ]    #     del modUrl2 [-endNum+saveNum : ]
+
     #打印修改后的列表
-    print (f'{modUrl1}')
-    print (f'{modUrl2}')
+    modUrl1 = "".join (modUrl1)
+    modUrl2 = "".join (modUrl2)
+    print (modUrl1)
+    print (modUrl2)
 
 if __name__ == '__main__':
-    url1 = 'happy1'
-    url2 = 'happ22'
-    modUrl1 = list(url1)
-    modUrl2 = list(url2)
+    # url1 = 'happy2happy'
+    # url2 = 'happ23happy'
+    url1 = input("请输入网址1：")
+    url2 = input("请输入网址2：")
+    tools_checkDif(url1, url2)
 
